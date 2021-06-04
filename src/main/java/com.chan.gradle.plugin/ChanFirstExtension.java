4fa60860433ef0805aeb3e44225ce2d86fded7ec
chandra-1765 <chandrakumar.selvaraj@zohocorplcom>
2021-06-04 11:04:32 +0530

    Java Files removed

--- src/main/java/com.chan.gradle.plugin/ChanFirstExtension.java
+++
@@ -1,8 +0,0 @@
-package com.chan.gradle.plugin;
-
-import org.gradle.api.provider.Property;
-
-abstract public class ChanFirstExtension {
-    abstract public Property<String> getFirstString();
-    abstract public Property<String> getSecondString();
-}
